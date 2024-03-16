8ad08a7578ee4f79444216e5f47c576df1ef056a
dmc mcc <mccarthydonal@hotmail.com>
2024-03-16 19:25:02 +0000

    add styling

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views import generic
 from .models import Post
@@ -11,6 +11,29 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.all().filter(status=1).order_by("-created_on")
-#    template_name = "post_list.html"
-# status = 1 : not in draft 
-      
+    template_name = "blog/index.html"
+    paginate_by = 6
+
+# note: status = 1 : not in draft 
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
+  
