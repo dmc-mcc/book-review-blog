a0c23a54daad8ff9b37dad403de630b7b7826270
dmc mcc <mccarthydonal@hotmail.com>
2024-03-21 20:51:43 +0000

    maint: some tidying up

--- blog/models.py
+++ blog/models.py
@@ -10,11 +10,9 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts", default='DMC')
-#    featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-#    excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     class Meta:
 #        ordering = ["-created_on", "author"]
